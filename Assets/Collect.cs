e8edcf727b1e8b9f03de75b222e64a2acc5147b2
Artur Dębkowski <arturdebkowski@gmail.com>
2017-03-16 12:33:34 +0100

    Outline shader

--- Assets/Collect.cs
+++ Assets/Collect.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class Collect : MonoBehaviour {
 
+    public bool isStaringAtCollectable;
+
+    private Shader standard, outlined;
+
 	// Use this for initialization
 	void Start () {
-		
+		standard = Shader.Find("Standard");
+        outlined = Shader.Find("Outlined/Silhouette Only");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-            if (Input.GetMouseButtonDown(0)){
-                RaycastHit hit;
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                var select = GameObject.FindWithTag("Collectable").transform;
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var collectable = GameObject.FindWithTag("Collectable").transform;
 
-                if (Physics.Raycast (ray, out hit, 10f)){
-                    Debug.DrawRay (ray.origin, hit.point);
-                    Debug.Log("Hit! at " + hit.point);
-                    //select.tag = "none";
-                    Debug.Log(hit);
-                    Debug.Log(hit.transform.tag);
-                    //hit.collider.transform.tag = "select";
-                }
-            }
-		
+        if (Physics.Raycast (ray, out hit, 10f) && !isStaringAtCollectable) {
+            isStaringAtCollectable = true;
+            // renderer.material.shader = outlined;
+
+            // Debug.DrawRay (ray.origin, hit.point);
+            // Debug.Log("Hit! at " + hit.point);
+            // Debug.Log(hit);
+            // Debug.Log(hit.transform.tag);
+
+            //collectable.tag = "none";
+            //hit.collider.transform.tag = "select";
+        } else {
+            isStaringAtCollectable = false;
+            // renderer.material.shader = standard;
+        }
+
+
+        if (Input.GetMouseButtonDown(0)){
+            // todo: add to players inventory            
+        }
 	}
 }
